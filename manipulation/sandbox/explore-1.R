8866fe6688e8b2060abe506cd41a1e89e5249f4f
Will Beasley <wibeasley@hotmail.com>
2020-04-03 00:56:23 -0500

    more exploring oecd w/ @andkov
    ref #7

--- manipulation/sandbox/explore-1.R
+++ manipulation/sandbox/explore-1.R
@@ -1,37 +1,28 @@
 # remotes::install_github("expersso/OECD")
 library(OECD)
-
+library(magrittr)
+config <- config::get()
 ds_list <- tibble::as_tibble(OECD::get_datasets())
 
 dataset <- "DUR_D"
 dstruc <- OECD::get_data_structure(dataset)
 str(dstruc, max.level = 2)
 # dput(dstruc$AGE$id) #c("1519", "1524", "2024", "2554", "5564", "6599", "900000")
-# dput(dstruc$COUNTRY$id)
-# c("AUS", "AUT", "BEL", "CAN", "CZE", "DNK", "EST", "FIN", "FRA",
-# "DEU", "GRC", "HUN", "ISL", "IRL", "ISR", "ITA", "JPN", "KOR",
-# "LVA", "LTU", "LUX", "MEX", "NLD", "NZL", "NOR", "POL", "PRT",
-# "SVK", "SVN", "ESP", "SWE", "CHE", "TUR", "GBR", "USA", "OECD",
-# "CRI", "EU22", "EU16", "EU28", "EUR", "G7", "NAM", "OCE", "BGR",
-# "HRV", "CYP", "MKD", "MLT", "ROU", "RUS", "ZAF", "COL")
 
-ages <- c("1519", "2024", "2554", "5564", "6599")
-countries <- c(
-  "AUS", "AUT", "BEL", "CAN", "CZE", "DNK", "EST", "FIN", "FRA",
-"DEU", "GRC", "HUN", "ISL", "IRL", "ISR", "ITA", "JPN", "KOR",
-"LVA", "LTU", "LUX", "MEX", "NLD", "NZL", "NOR", "POL", "PRT",
-"SVK", "SVN", "ESP", "SWE", "CHE", "TUR", "GBR", "USA", "OECD",
-"CRI", "EU22", "EU16", "EU28", "EUR", "G7", "NAM", "OCE", "BGR",
-"HRV", "CYP", "MKD", "MLT", "ROU", "RUS", "ZAF", "COL")
-# cat(paste(dstruc$COUNTRY$id, collapse = "\n"))
 # readr::write_csv(dstruc$COUNTRY, "data-public/metadata/oecd/country.csv")
 
+
+#### ellis starts here
+ds_country <-
+  readr::read_csv(
+    config$path_country
+  ) %>%
+  dplyr::filter(desired)
+
 filter_list <- list(
-  c("DEU", "FRA"),
-  # "MW",
-  c("MEN", "WOMEN"),
-  # "900000"
-  ages
+  ds_country$id,
+  config$oced_genders,
+  config$oced_ages
 )
 df <-
   OECD::get_dataset(
