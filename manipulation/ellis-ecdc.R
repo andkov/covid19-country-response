388d31702bc78f4df0e53156ef54813d8ef2e2dd
Andriy V. Koval <koval.andrey@gmail.com>
2020-04-04 13:26:37 -0400

    Update ellis-ecdc.R
    #9

--- manipulation/ellis-ecdc.R
+++ manipulation/ellis-ecdc.R
@@ -1,14 +1,47 @@
-#these libraries need to be loaded
+# Source
+# https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide
+
+
+
+rm(list=ls(all=TRUE)) #Clear the memory of variables from previous run.
+# This is not called by knitr, because it's above the first chunk.
+cat("\f") # clear console when working in RStudio
+
+# ---- load-sources ------------------------------------------------------------
+# Call `base::source()` on any repo file that defines functions needed below.  Ideally, no real operations are performed.
+
+# ---- load-packages -----------------------------------------------------------
+# Attach these packages so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+library(magrittr) #Pipes
 library(utils)
 library(httr)
 library(magrittr)
 library(dplyr)
 
-# Source
-# https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide
+# ---- declare-globals ---------------------------------------------------------
+path_url <- "https://opendata.ecdc.europa.eu/covid19/casedistribution/csv"
+# ---- load-data ---------------------------------------------------------------
 
 #download the dataset from the ECDC website to a local temporary file
-GET("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", authenticate(":", ":", type="ntlm"), write_disk(tf <- tempfile(fileext = ".csv")))
+# GET(url = path_url, authenticate(":", ":", type="ntlm"), write_disk(tf <- tempfile(fileext = ".csv")))
+# ds_ocdc_raw <- read.csv(tf)
+path_save <- paste0("./data-unshared/derived/ocdc-",Sys.Date(),".csv")
+# readr::write_csv(ds_ocdc_raw,path_save)
+# run above line once per update
+
+ds0 <- readr::read_csv(path_save)
+
+
+# ---- tweak-data -----------------------
+ds <- ds_ocdc_raw
+
+# ---- define-utility-functions ---------------
+
+# ---- save-to-disk ----------------------------
+
+
+
+
 
 #read the Dataset sheet into “R”. The dataset will be called "data".
 ds <- read.csv(tf)
@@ -20,7 +53,6 @@ names(ds) <- c("date", "day", "month", "year", "n_cases", "n_deaths", "country",
 ds <- ds %>%
   dplyr::mutate(
     date = lubridate::dmy(date)
-    # dd =
   )
 ds %>% glimpse()
 
