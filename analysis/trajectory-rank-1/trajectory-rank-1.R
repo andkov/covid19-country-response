a06bd45a49ed824210fdcda86961aefc3a2d83cb
Andriy V. Koval <koval.andrey@gmail.com>
2020-04-04 21:01:08 -0400

    data inputs ready
    #5

--- analysis/trajectory-rank-1/trajectory-rank-1.R
+++ analysis/trajectory-rank-1/trajectory-rank-1.R
@@ -19,21 +19,61 @@ requireNamespace("DT", quietly=TRUE) # for dynamic tables
 # requireNamespace("mgcv, quietly=TRUE) #For the Generalized Additive Model that smooths the longitudinal graphs.
 
 # ---- load-sources ------------------------------------------------------------
-#Load any source files that contain/define functions, but that don't load any other types of variables
-#   into memory.  Avoid side effects and don't pollute the global environment.
+config <- config::get()
 source("./scripts/common-functions.R")        # reporting functions and quick views
 source("./scripts/graphing/graph-presets.R") # font and color conventions
 source("./scripts/graphing/graph-support.R") # font and color conventions
 
 # ---- load-data -------------------------------------------------------------
+ds_country <-
+  readr::read_csv(
+    config$path_country
+  ) %>%
+  dplyr::filter(desired)
 
+# COVID
+path_save <- paste0("./data-unshared/derived/ocdc-",Sys.Date(),".csv")
+ds_covid <- readr::read_csv(path_save)
+ds_covid %>% glimpse()
+
+# OEDC
+path_folder_oecd_health <- "./data-unshared/raw/oecd/health/"
+input_files_oecd_health <- list.files(path_folder_oecd_health, pattern = ".rds$",  full.names = T)
+file_names <- gsub(".rds$","", basename(input_files_oecd_health))
+
+ls_input_health <- list()
+for(i in seq_along(input_files_oecd_health)){
+  file_name_i <- gsub(".rds$","", basename(input_files_oecd_health[i]))
+  ls_input_health[[file_name_i]] <- readr::read_rds(input_files_oecd_health[i])
+}
 # ---- inspect-data -------------------------------------------------------------
 
 # ---- tweak-data --------------------------------------------------------------
+d_covid <- ds_covid %>%
+  # dplyr::select(country_code, date, n_deaths) %>%
+  dplyr::filter(country_code %in% unique(ds_country$id)) %>%
+  dplyr::group_by(country_code) %>%
+  dplyr::mutate(
+    # this solution might be vulnerable to cases where some intermediate dates are missed
+    n_deaths_cum = cumsum(n_deaths)
+    ,cutoff = n_deaths_cum > 0
+    ,epi_timeline = cumsum(cutoff)
+  ) %>%
+  dplyr::ungroup() %>%
+  dplyr::filter(epi_timeline > 0)
+d_covid %>% print(n = nrow(.))
 
 # ---- basic-table --------------------------------------------------------------
 
-# ---- basic-graph --------------------------------------------------------------
+# ---- basic-graph -------------------------------------------------------------
+d_meta <- ls_input_health$health_resources %>% get_var_unit_lookup()
+
+var_name <- "HOPITBED"
+unit_name     <- "RTOINPNB"
+
+d_measure <- ls_input_health$health_resources %>% compute_rank(var_name, unit_name)
+
+
 
 # Sonata form report structure
 # ---- dev-a-0 ---------------------------------
