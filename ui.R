beaaecc102d4cd5e6d4f2a3896378a844d4b54b3
Andriy V. Koval <koval.andrey@gmail.com>
2020-04-08 23:35:02 -0400

    put hardcoded choiced in separate file

--- ui.R
+++ ui.R
@@ -9,7 +9,7 @@
 
 library(shiny)
 library(plotly)
-
+base::source("./scripts/available_labels.R")
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
@@ -22,47 +22,22 @@ shinyUI(fluidPage(
             selectInput(
                 inputId='var_name',
                 label='Variable',
-                choices=c(
-                    "CARECPRA", "CARECPRO", "DNSTDENT", "DNSTLPDN", "DNSTPADN",
-                    "EMPLGENE", "EMPLGENP", "EMPLGYNE", "EMPLOTGP", "EMPLOTPH", "EMPLOTSP",
-                    "EMPLPEDI", "EMPLPSYS", "EMPLSPEC", "EMPLSPMP", "EMPLSURG", "GEHETHSM",
-                    "HEDUAPGR", "HEDUDNGR", "HEDUMEGR", "HEDUMWGR", "HEDUNUGR", "HEDUPHGR",
-                    "HEDUPNGR", "HOEMHASN", "HOEMHEMP", "HOEMHHCA", "HOEMHNUR", "HOEMHOTH",
-                    "HOEMHOTS", "HOEMHPHY", "HOPIFROH", "HOPILICS", "HOPILIMM", "HOPILIPS",
-                    "HOPINROH", "HOPIOBED", "HOPIREHA", "HOPITBED", "HOPITPOH", "HOSPFRHO",
-                    "HOSPGHOS", "HOSPNPHO", "HOSPPUHO", "HOSPTHOS", "IPINGACA", "IPINGAMA",
-                    "IPINGAMH", "IPINMAMA", "IPINMAMH", "IPINMAMO", "IPINMRIA", "IPINMRIH",
-                    "IPINMRIM", "IPINPETA", "IPINPETH", "IPINPETS", "IPINRTEA", "IPINRTEH",
-                    "IPINRTEQ", "IPINSCAA", "IPINSCAH", "IPINSCAN", "MIDWMIDW", "MIDWMILP",
-                    "MIDWMIPA", "MINUASSO", "MINUINFI", "MINULPAP", "MINULPPN", "MINUNULP",
-                    "MINUPAAP", "MINUPANU", "MINUPAPN", "MINUQUAL", "PAGGF344", "PAGGF454",
-                    "PAGGF564", "PAGGF65O", "PAGGF75O", "PAGGFEMM", "PAGGFU35", "PAGGHOMM",
-                    "PAGGM344", "PAGGM454", "PAGGM564", "PAGGM65O", "PAGGM75O", "PAGGMU35",
-                    "PAGGT344", "PAGGT454", "PAGGT564", "PAGGT65O", "PAGGT75O", "PAGGTOPY",
-                    "PAGGTU35", "PHSTLPPH", "PHSTPAPH", "PHSTPHAR", "PHYSMEDE", "PHYSPAPS",
-                    "PHYSREGP", "PSIOPHTH", "RVNURINF", "RVNURMED", "RVNURMEG"
-                ),
+                choices=var_name_available,
+                # choices=var_label_available,
                 selected=1
             ),
             selectInput(
                 inputId='unit_name',
                 label='Unit Name',
-                choices=c(
-                    "DENSPPNB", "PERSMYNB", "PERMEDNB", "MILBIRNB", "PEREMPNB",
-                    "NOMBRENB", "RTOALLNB", "PERPHYNB", "DENSEFEF", "FTEEMPEF", "PFHEMPEF",
-                    "PTHEMPNB", "RTOINPNB", "LTCOVRNB", "HOPFTE", "HOPRAT", "NURFTE",
-                    "NURRAT", "NBMILPNB", "PHY344NB", "PHY454NB", "PHY564NB", "PHYT65NB",
-                    "PHYT75NB", "PHYTOTNB", "PHYU35NB", "YSALARMT", "YSALAWMT", "YSALGDMT",
-                    "YSALPIMT", "YSALPPMT", "YSALXRMT", "YSEFAWMT", "YSEFGDMB", "YSEFPIMT",
-                    "YSEFPPMB", "YSEFXRMB", "YSELFPMB"
-                ),
+                # choices=unit_label_available,
+                choices=unit_name_available,
                 selected=1
-            ),
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            )#,
+            # sliderInput("bins",
+            #             "Number of bins:",
+            #             min = 1,
+            #             max = 50,
+            #             value = 30)
         ),
 
         # Show a plot of the generated distribution
