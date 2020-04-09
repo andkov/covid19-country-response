c53924a85e4aea7d9bf5b2b252b0b476965bb321
Andriy V. Koval <koval.andrey@gmail.com>
2020-04-08 21:30:35 -0400

    removed utility

--- utility/super-computer/hello-world.R
+++
@@ -1,6 +0,0 @@
-# See documentation in
-#   * https://github.com/wibeasley/RAnalysisSkeleton/tree/master/stitched-output/utility/super-computer
-#   and
-#   * https://github.com/wibeasley/RAnalysisSkeleton/blob/master/documentation/oscer-steps.md
-
-print("hello world")
--- utility/super-computer/repo.R
+++
@@ -1,6 +0,0 @@
-# dir.create("stitched-output/utility")
-
-knitr::stitch_rmd(
-  script = "utility/reproduce.R", 
-  output = "stitched-output/utility/reproduce.md"
-)
