e20f5f05eb1dbcd89daac1571772e86787967404
Andriy V. Koval <koval.andrey@gmail.com>
2020-05-03 09:26:10 -0400

    Update common-functions.R

--- scripts/common-functions.R
+++ scripts/common-functions.R
@@ -137,13 +137,12 @@ compute_epi_timeline <- function(d, n_deaths_first_day = 1) { #}, d_country ){
     dplyr::filter(desired)
 
   d_out <- d %>%
-    # dplyr::select(country_code, date, n_deaths) %>%
     dplyr::filter(country_code %in% unique(d_country$id)) %>%
     dplyr::group_by(country_code) %>%
     dplyr::mutate(
       # this solution might be vulnerable to cases where some intermediate dates are missed
-      n_deaths_cum = cumsum(n_deaths)
-      ,cutoff = n_deaths_cum > n_deaths_first_day
+      n_deaths_cum  = cumsum(n_deaths)
+      ,cutoff       = n_deaths_cum > n_deaths_first_day
       ,epi_timeline = cumsum(cutoff)
     ) %>%
     dplyr::ungroup() %>%
