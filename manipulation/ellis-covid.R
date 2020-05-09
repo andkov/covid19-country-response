4d9ca38aa5dced5b0149cd924d1d187c11ac2e5b
Andriy V. Koval <koval.andrey@gmail.com>
2020-05-08 22:22:42 -0400

    Update ellis-covid.R

--- manipulation/ellis-covid.R
+++ manipulation/ellis-covid.R
@@ -53,11 +53,38 @@ ds_country <-
 # sources can be joined by the three letter country code
 # ---- tweak-data -----------------------
 names(ds_covid) <- c("date", "day", "month", "year", "n_cases", "n_deaths", "country", "geo_id", "country_code","n_population_2018", "continent")
+
 ds_covid <- ds_covid %>%
   dplyr::mutate(
     date = lubridate::dmy(date)
   ) %>%
+  dplyr::select(
+    country_code,
+    date,
+    n_cases,
+    n_deaths,
+    n_population_2018
+  )
+ds_covid %>% glimpse()
+# stem to have the complete timeline
+dates     <- min(ds_covid$date):max(ds_covid$date)
+countries <- unique(ds_covid$country_code) %>% na.omit()
+ds_dates   <- tibble::as_tibble(
+  expand.grid(dates, countries, stringsAsFactors = F)
+)
+names(ds_dates) <- c("date", "country_code")
+ds_dates <- ds_dates %>%
+  dplyr::mutate(
+    date = lubridate::as_date(date)
+  )
+ds_dates %>% glimpse()
+ds_covid %>% glimpse()
+
+ds_covid <- ds_dates %>%
+  dplyr::left_join(ds_covid,by = c("date","country_code") ) %>%
+  tidyr::fill(n_population_2018) %>%
   dplyr::arrange(country_code, date)
+
 
 readr::write_csv(ds_covid, config$path_input_covid)
 
