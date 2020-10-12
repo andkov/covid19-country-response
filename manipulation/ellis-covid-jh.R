b0c15b1c57d4a2c2c556c8bd1a856ea18683bc45
Andriy V. Koval <koval.andrey@gmail.com>
2020-10-12 15:03:03 -0400

    Update ellis-covid-jh.R

--- manipulation/ellis-covid-jh.R
+++ manipulation/ellis-covid-jh.R
@@ -22,7 +22,8 @@ library(stringr)
 # ---- declare-globals ---------------------------------------------------------
 config <- config::get()
 folder_daily_reports <- "../COVID-19/csse_covid_19_data/csse_covid_19_daily_reports_us/"
-
+path_timeseries_cases_us <- "../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
+path_timeseries_deaths_us <- "../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"
 # ---- load-data ---------------------------------------------------------------
 daily_reports <- list.files(folder_daily_reports,pattern = "*.csv", full.names = T)
 ls_daily <- list()
@@ -34,16 +35,49 @@ for(i in seq_along(daily_reports)){
   ls_daily[[as.character(i_name)]] <- readr::read_csv(daily_reports[i])
 
 }
+ds_cases <- readr::read_csv(path_timeseries_cases_us)
+ds_deaths <- readr::read_csv(path_timeseries_deaths_us)
+
+ds_cases %>% glimpse()
+ds_deaths %>% glimpse()
+# ---- tweak-data -------------------
+
+(stem_cases <- names(ds_cases)[1:11])
+(stem_deaths <- names(ds_deaths)[1:12])
+
+
+long_cases <- setdiff(names(ds_cases), stem_cases)
+long_deaths <- setdiff(names(ds_deaths), stem_deaths)
+
+ds_cases_long <- ds_cases %>%
+  tidyr::pivot_longer(all_of(long_cases), names_to = "date", values_to = "n_cases") %>%
+  dplyr::mutate(
+    date = lubridate::mdy(date)
+  )
+
+ds_cases_long %>% glimpse()
+
+
+ds_deaths_long <- ds_deaths %>%
+  tidyr::pivot_longer(all_of(long_deaths), names_to = "date", values_to = "n_deaths") %>%
+  dplyr::mutate(
+    date = lubridate::mdy(date)
+  )
+ds_deaths_long %>% glimpse()
+
+ds_timeseries <-
+  left_join(ds_deaths_long, ds_cases_long) %>%
+  janitor::clean_names()
+
+ds_timeseries %>% glimpse()
+
 
 ds_daily <- ls_daily %>% bind_rows(.id = "date") %>% janitor::clean_names()
-
 ds_daily %>% glimpse()
 
 
-us_pop <- readr::read_csv("../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv") %>% janitor::clean_names() %>% select(1:12)
-
+us_pop <- ds_deaths %>% janitor::clean_names() %>% select(1:12)
 us_pop %>%  filter(country_region == "US") %>% glimpse()
-
 ds_uspop <- us_pop %>%
   group_by(province_state) %>%
   summarize(
@@ -53,8 +87,12 @@ ds_uspop <- us_pop %>%
 ds_daily <- ds_daily %>%
   left_join(ds_uspop,by = "province_state")
 
+
+
+
 # ---- save-to-disk --------------------
 ds_daily %>% readr::write_csv(config$path_input_jh_daily)
+ds_timeseries %>% readr::write_csv(config$path_input_jh_usts)
 # ds_daily %>% readr::write_csv("./data-unshared/derived/js_daily.rds")
 
 
