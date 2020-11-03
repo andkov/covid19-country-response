582c9fda51c32a639967975afe003aa5f1ef83b9
Kyle Belanger <kyleb44@hotmail.com>
2020-11-02 19:46:33 -0500

    update app
    #19
    App displays staic image

--- analysis/shiny-covid-votes/server.R
+++ analysis/shiny-covid-votes/server.R
@@ -6,9 +6,152 @@ cat("\f") # clear console when working in RStudio
 library(shiny)
 library(tidyverse)
 
-# ---- load-sources ------------------------------------------------------------
+#---- load-sources ------------------------------------------------------------
+config <- config::get()
+# Use 2nd line for local testing
+source("../../scripts/common-functions.R")  # reporting functions and quick views
+# source("./scripts/common-functions.R")  # reporting functions and quick views
+
+
+# ---- declare-globals --------------------
+# for running app
+dir_path <- "../../"
+
+# for local testing
+# dir_path <- "./"
+
+ggplot2::theme_set(
+    ggplot2::theme_bw(
+    )+
+        theme(
+            strip.background = element_rect(fill="grey90", color = NA)
+        )
+)
+
+division_levels <- c(
+    "Pacific"
+    ,"Mountain"
+    ,"New England"
+    ,"West North Central"
+    ,"East North Central"
+    ,"Middle Atlantic"
+    ,"West South Central"
+    ,"South Atlantic"
+    ,"East South Central"
+)
+
+region_colors <- c(
+    "West" = "#1B9E77"
+    ,"South" = "#E7298A"
+    ,"NorthEast" = "#7570B3"
+    ,"MidWest" = "#D95F02"
+)
+
+party_colors <- c(
+    "Democrat"    = "#0015BC"
+    ,"Divided"    = "#800080"
+    ,"Republican" = "#E9141D"
+
+)
+
+# ---- declare-functions ---------------------------
+metric_order <- c(
+    "n_cases_roll_7"         = "Cases (7-day average)"
+    ,"n_cases_roll_7_rate"   = "Cases (7DA/100K)"
+    ,"n_cases_cum"           = "Cases (cumulative)"
+    ,"incident_rate"         = "Cases (cum/100K)"
+    ,"n_deaths_roll_7"       = "Deaths (7-day average)"
+    ,"n_deaths_roll_7_rate"  = "Deaths (7DA/100K)"
+    ,"n_deaths_cum"          = "Deaths (cumulative)"
+    ,"mortality_rate"        = "Deaths (cum/100K)"
+    ,"n_tests_roll_7"        = "Tests (7-day average)"
+    ,"n_tests_roll_7_rate"   = "Tests (7DA/100K)"
+    ,"n_tests_cum"           = "Tests (cumulative)"
+    ,"testing_rate"          = "Tests (cum/100K)"
+)
+
+compute_epi <- function(
+    d
+    ,grouping_vars
+    ,var_cases = "n_cases"
+    ,var_deaths = "n_deaths"
+    ,var_tests = "n_tested"
+    ,long =FALSE){
+    # d <- ds_jh_state %>% filter(province_state == "Florida")
+    # grouping_vars <- c("date","province_state")
+    # grouping_vars <- c("date")
+    # var_cases = "n_cases"
+    # var_deaths = "n_deaths"
+    # var_tests = "people_tested"
+    # long = F
+    # browser()
+    grouping_vars_enquo <- rlang::syms(grouping_vars)
+    grouping_vars_no_date <- rlang::syms(setdiff(grouping_vars,"date"))
+    var_cases_enquo  <- rlang::sym(var_cases)
+    var_deaths_enquo <- rlang::sym(var_deaths)
+    var_tests_enquo  <- rlang::sym(var_tests)
+
+
+    d_out <- d %>%
+        dplyr::arrange(!!!grouping_vars_enquo) %>%
+        dplyr::group_by(!!!grouping_vars_enquo) %>%
+        dplyr::summarize(
+            n_cases_cum     = sum(!!var_cases_enquo, na.rm = T)
+            ,n_deaths_cum   = sum(!!var_deaths_enquo, na.rm = T)
+            ,n_tests_cum    = sum(!!var_tests_enquo, na.rm = T)
+            ,population     = sum(population, na.rm = T)
+            ,incident_rate  = n_cases_cum/population*100000
+            ,mortality_rate = n_deaths_cum/population*100000
+            ,testing_rate   = n_tests_cum/population*100000
+            ,.groups = "keep"
+        ) %>%
+        group_by(!!!grouping_vars_no_date) %>%
+        arrange(date) %>%
+        dplyr::mutate(
+            n_cases   = n_cases_cum - lag(n_cases_cum,1)
+            ,n_deaths  = n_deaths_cum - lag(n_deaths_cum,1)
+            ,n_tests    = n_tests_cum - lag(n_tests_cum,1)
+            ,n_cases_roll_7 = zoo::rollapply(n_cases, 7, mean, align = 'right', fill = NA)
+            ,n_deaths_roll_7 = zoo::rollapply(n_deaths, 7, mean, align = 'right', fill = NA)
+            ,n_tests_roll_7 = zoo::rollapply(n_tests, 7, mean, align = 'right', fill = NA)
+            ,n_cases_roll_7_rate = n_cases_roll_7/population*100000
+            ,n_deaths_roll_7_rate = n_deaths_roll_7/population*100000
+            ,n_tests_roll_7_rate = n_tests_roll_7/population*100000
+        ) %>%
+        ungroup() %>%
+        select(all_of(c(grouping_vars,names(metric_order))))
+    # d_out %>% glimpse()
+    if(long){
+        var_pivot_longer <- setdiff(names(d_out), grouping_vars)
+        d_out <- d_out %>%
+            tidyr::pivot_longer(cols = var_pivot_longer, names_to = "metric", values_to = "value") %>%
+            mutate(
+                metric = factor(metric, levels = names(metric_order), labels = metric_order)
+            )
+    }
+    d_out <- d_out %>% dplyr::na_if(0L)
+    return(d_out)
+}
 
 # ---- load-data ---------------------------------------------------------------
+# Produced by `./manipulation/scribe-john-hopkins.R`
+ds_jh_state <- readr::read_rds(
+    paste0(dir_path,"data-unshared/derived/john-hopkins-state.rds")
+    )
+# Source: Harvard Datavers (presidential) + Kaiser Foundation (state parties)
+# Produced by `./manipulation/ellis-us-election-results-2.R`
+ds_vote <- readr::read_rds(
+    paste0(dir_path,"data-public/derived/us-2020-state-political-results.rds")
+    )
+# Note: political leadership reflects the state of 2020
+
+ds_covid_vote <- ds_jh_state %>%
+    left_join(
+        ds_vote %>% select(-c("state_po","state_fips"))
+        ,by = c("state"= "province_state")
+    ) %>%
+    mutate(state = factor(state))
+
 
 
 
@@ -16,16 +159,63 @@ library(tidyverse)
 # ---- shiny-server ------------------------------------------------------------
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
+    # TODO update to allow user input
+    focal_dates <- as.Date(c(
+        "2020-10-27"
+        ,"2020-10-15","2020-10-01"
+        ,"2020-09-15","2020-09-01"
+        ,"2020-08-15","2020-08-01"
+        ,"2020-07-15","2020-07-01"
+        ,"2020-06-15","2020-06-01"
+        ,"2020-05-15","2020-05-01"
+        ,"2020-04-15","2020-04-01"
+        ,"2020-03-15"
+    ))
 
-    output$distPlot <- renderPlot({
+    political_grouping <- "winner_2016"
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    d <- ds_covid_vote %>%
+        compute_epi(
+            c(
+                "date"
+                ,"state", "state_abb"
+                ,"division"
+                ,"region"
+                ,"country"
+                ,political_grouping
+            ), long = T)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    # TODO update to allow user to chose focus
+    focus_metric <- c("Cases (7DA/100K)","Cases (cum/100K)")
 
-    })
+    d4 <- d %>%
+        filter(date %in% focal_dates) %>%
+        filter(metric %in% focus_metric) %>%
+        mutate(
+            metric = janitor::make_clean_names(as.character(metric))
+            ,metric = str_remove_all(metric, "_\\d+$")
+        ) %>%
+        tidyr::pivot_wider(names_from = "metric", values_from = "value")
+
+    g4 <- d4 %>%
+        ggplot(
+            aes_string(
+                x=janitor::make_clean_names(focus_metric[1])
+                , y =janitor::make_clean_names(focus_metric[2])
+                ,label = "state_abb"
+                ,fill = political_grouping
+                ,color = political_grouping
+            ))+
+        scale_fill_manual(values = config$party_colors)+
+        scale_color_manual(values = config$party_colors)+
+        geom_point(shape = 21, color = "grey30",alpha = .2, size = 7)+
+        geom_text(alpha = .9, size = 3)+
+        facet_wrap(~date, scales = "free_y")+
+        labs(x = focus_metric[1], y = focus_metric[2])
+
+
+    output$plot1 <- renderPlot(g4)
+
+
 
 })
--- analysis/shiny-covid-votes/ui.R
+++ analysis/shiny-covid-votes/ui.R
@@ -13,21 +13,7 @@ library(shiny)
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("COVID-19 Votes"),
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
+    plotOutput("plot1")
 ))
