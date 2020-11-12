93cdcd0e7f9e813d796f7902477a92d52049540d
Andriy V. Koval <koval.andrey@gmail.com>
2020-11-12 13:05:25 -0500

    Update ellis-covid-jh.R
    #21
    Converted into long and join cases and deaths, but adding population needs more work - no province_state level

--- manipulation/ellis-covid-jh.R
+++ manipulation/ellis-covid-jh.R
@@ -27,7 +27,17 @@ path_timeseries_deaths_us <- "../COVID-19/csse_covid_19_data/csse_covid_19_time_
 
 path_timeseries_cases_global <- "../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 path_timeseries_deaths_global <- "../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+
+
+path_source_pop <- paste0(
+  "https://population.un.org/wpp/Download/Files/"
+  ,"1_Indicators%20(Standard)/CSV_FILES/WPP2019_TotalPopulationBySex.csv"
+)
+
 # ---- load-data ---------------------------------------------------------------
+ds_world_pop <-  read_csv(path_source_pop)
+
+
 daily_reports <- list.files(folder_daily_reports,pattern = "*.csv", full.names = T)
 ls_daily <- list()
 for(i in seq_along(daily_reports)){
@@ -51,7 +61,32 @@ ds_cases_global[1:13] %>% glimpse()
 ds_deaths_global[1:13] %>% glimpse()
 
 
-# ---- tweak-data -------------------
+# ---- tweak-data ------------
+ds_world_pop1 <- ds_world_pop %>%
+  filter(Time == 2020, VarID == 2) %>%
+  select(LocID,Location,Time,PopTotal) %>%
+  mutate(across(PopTotal, ~.*1000))
+ds_world_pop1 %>% glimpse()
+
+# ---- tweak-data-daily -----------------------
+ds_daily <- ls_daily %>% bind_rows(.id = "date") %>% janitor::clean_names()
+ds_daily %>% glimpse()
+
+
+us_pop <- ds_deaths %>% janitor::clean_names() %>% select(1:12)
+us_pop %>%  filter(country_region == "US") %>% glimpse()
+ds_uspop <- us_pop %>%
+  group_by(province_state) %>%
+  summarize(
+    population = sum(population, na.rm = T)
+  ) %>%
+  ungroup()
+# add state population
+ds_daily <- ds_daily %>%
+  left_join(ds_uspop,by = "province_state")
+
+
+# ---- tweak-data-timeseries-us -------------------
 
 (stem_cases <- names(ds_cases)[1:11])
 (stem_deaths <- names(ds_deaths)[1:12])
@@ -86,24 +121,52 @@ ds_timeseries <-
   janitor::clean_names()
 
 ds_timeseries %>% glimpse()
+# ds_timeseries %>% distinct(province_state, country_region) %>% View()
+
+# --- tweak-data-timeseries-global ---------------
+(stem_cases_global <- names(ds_cases_global)[1:4])
+(stem_deaths_global <- names(ds_deaths_global)[1:4])
+
+long_cases_global <- setdiff(names(ds_cases_global), stem_cases_global)
+long_deaths_global <- setdiff(names(ds_deaths_global), stem_deaths_global)
 
 
-ds_daily <- ls_daily %>% bind_rows(.id = "date") %>% janitor::clean_names()
-ds_daily %>% glimpse()
+ds_cases_global_long <- ds_cases_global %>%
+  tidyr::pivot_longer(all_of(long_cases_global), names_to = "date", values_to = "n_cases") %>%
+  dplyr::mutate(
+    date = lubridate::mdy(date)
+  )
+
+ds_cases_global_long %>% glimpse()
 
 
-us_pop <- ds_deaths %>% janitor::clean_names() %>% select(1:12)
-us_pop %>%  filter(country_region == "US") %>% glimpse()
-ds_uspop <- us_pop %>%
-  group_by(province_state) %>%
-  summarize(
-    population = sum(population, na.rm = T)
-  ) %>%
-  ungroup()
-# add state population
-ds_daily <- ds_daily %>%
-  left_join(ds_uspop,by = "province_state")
+ds_deaths_global_long <- ds_deaths_global %>%
+  tidyr::pivot_longer(all_of(long_deaths_global), names_to = "date", values_to = "n_deaths") %>%
+  dplyr::mutate(
+    date = lubridate::mdy(date)
+  )
+ds_deaths_global_long %>% glimpse()
 
+ds_timeseries_global <-
+  left_join(ds_cases_global_long, ds_deaths_global_long) %>%
+  janitor::clean_names()
+
+ds_timeseries_global %>% glimpse()
+
+ds_timeseries_global %>% filter(country_region == "United Kingdom")
+
+# The population adds the country_region level, but not province_state
+d <- ds_timeseries_global %>%
+  left_join(
+    ds_world_pop1 %>% select(Location, population = PopTotal)
+    , by = c("country_region" = "Location")) %>%
+
+
+# d <- ds_timeseries_global %>%
+#   left_join(
+#     ds_timeseries %>% distinct(date, province_state, country_region, long, lat)
+#   )
+# ds_timeseries_global %>% distinct(province_state, country_region) %>% View()
 
 # ----- global-timeseries ------------
 # TODO: please create a `ds_timeseries_global` that would include US and list population for each country
