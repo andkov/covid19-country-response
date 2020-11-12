f17658e76827f6a90217f919d7989575dc450001
Kyle Belanger <kyleb44@hotmail.com>
2020-11-11 21:38:41 -0500

    Update ellis-covid-who.R
    #22 Data source completed
    @andkov where should this be saved?

--- manipulation/ellis-covid-who.R
+++ manipulation/ellis-covid-who.R
@@ -2,32 +2,79 @@
 # https://covid19.who.int/table
 # must update local repository before recreating the analysis-ready data file
 
+# Population source WHO uses for shiny App
+# https://population.un.org/wpp/Download/Standard/Population/
+
 
 rm(list=ls(all=TRUE)) #Clear the memory of variables from previous run.
 # This is not called by knitr, because it's above the first chunk.
 cat("\f") # clear console when working in RStudio
 
 # ---- load-sources ------------------------------------------------------------
-# Call `base::source()` on any repo file that defines functions needed below.  Ideally, no real operations are performed.
+# Call `base::source()` on any repo file that defines functions needed below.
+# Ideally, no real operations are performed.
 
 
 # ---- load-packages -----------------------------------------------------------
-# Attach these packages so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
-library(magrittr) #Pipes
-library(utils)
-library(httr)
-library(magrittr)
-library(dplyr)
-library(stringr)
+# Attach these packages so their functions don't need to be qualified:
+# http://r-pkgs.had.co.nz/namespace.html#search-path
+library(tidyverse)
 
 # ---- declare-globals ---------------------------------------------------------
 config <- config::get()
-path_source <- "https://covid19.who.int/WHO-COVID-19-global-data.csv"
+path_source_who <- "https://covid19.who.int/WHO-COVID-19-global-data.csv"
+path_source_pop <- paste0(
+"https://population.un.org/wpp/Download/Files/"
+,"1_Indicators%20(Standard)/CSV_FILES/WPP2019_TotalPopulationBySex.csv"
+)
+
+column_names <- c(
+  "date"          = "Date_reported"
+  ,"iso2"          = "Country_code"
+  ,"country_label" = "Country"
+  ,"who_region"    = "WHO_region"
+  ,"n_cases"       = "New_cases"
+  ,"n_cases_cum"   = "Cumulative_cases"
+  ,"n_deaths"      = "New_deaths"
+  ,"n_deaths_cum"  = "Cumulative_deaths"
+)
 
 # ---- load-data ---------------------------------------------------------------
 # TODO: please process the data so it can be compared to other soruces
 # Please add the population
 
+ds_who <- read_csv(path_source_who)
+
+ds_world_pop <-  read_csv(path_source_pop)
 # ---- tweak-data -------------------
 
-# ---- save-to-disk --------------------
+ds_who1 <- ds_who %>% rename(all_of(column_names)) %>%
+  mutate(
+    across(date, ~ymd(.))
+  )
+
+# need to multiply pop by 1000 to move the decimal over, not sure why
+# file downloads with oddly placed decimal
+ds_world_pop1 <- ds_world_pop %>%
+  filter(Time == 2020, VarID == 2) %>%
+  select(LocID,Location,Time,PopTotal) %>%
+  mutate(across(PopTotal, ~.*1000))
+
+
+
+ds_who_combined <- ds_who1 %>%
+  left_join(ds_world_pop1, by = c("country_label" = "Location")) %>%
+  select(-Time, -LocID) %>%
+  rename(population = PopTotal)
+
+
+# ---- save-to-disk ------------------------------------------------------------
+
+# TODO: Where to save?
+
+
+
+
+
+
+
