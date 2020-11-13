624702388a49efbddbf2b621c61737d81f636348
Kyle Belanger <kyleb44@hotmail.com>
2020-11-13 08:38:18 -0500

    update 2020 ellis

--- manipulation/ellis-us-election-results_pres_2020.R
+++ manipulation/ellis-us-election-results_pres_2020.R
@@ -41,3 +41,52 @@ ds0 <- read_csv("./data-unshared/raw/vote/president_county_candidate_2020.csv")
 
 #' # Tweak Data
 # ---- tweak-data -------------------------------------------------------------
+
+ds_county <- ds0 %>% filter(party %in% c("DEM", "REP")) %>%
+  select(-won, -candidate) %>%
+  pivot_wider(
+    names_from     = party
+    ,values_from   = total_votes
+    , names_prefix = "votes_"
+    ) %>%
+  janitor::clean_names() %>%
+  mutate(
+    winner = if_else(
+      votes_rep > votes_dem
+      ,"Republican"
+      ,"Democrat"
+      )
+    )
+
+
+
+ds_state <- ds_county %>%
+  select(-winner) %>%
+  group_by(state) %>%
+  summarise(
+    votes_dem = sum(votes_dem)
+    ,votes_rep = sum(votes_rep)
+    ) %>%
+  mutate(
+    winner = if_else(
+      votes_rep > votes_dem
+      ,"Republican"
+      ,"Democrat"
+    )
+  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
